3631b44a4f6276e1017dd459055483bd76035598
aleksandr_savchenko <aleksandr_savchenko@epam.com>
2017-04-03 12:42:22 +0300

    docker-spark-streaming: added saving of data to Mongo

--- configuration-profiles/docker-spark-streaming/README.md
+++ configuration-profiles/docker-spark-streaming/README.md
@@ -18,7 +18,16 @@ Here are the steps to follow to get set-up streaming
 # check if application is up and running
 HTTP GET check: http://192.168.99.100:11091/server/test
 # run spark streaming
-docker exec -it web-app /usr/spark-2.1.0/bin/spark-submit  /opt/jar/target/spark-streaming-jar-module.jar
+docker exec -it web-app /usr/spark-2.1.0/bin/spark-submit \
+--jars /opt/mongo-spark-connector_2.11-2.0.0.jar,/opt/mongo-java-driver-3.4.2.jar \
+/opt/jar/target/spark-streaming-jar-module.jar
+
+# connect to mongo
+mongo --host 192.168.99.100 --port 30001
+# check received data
+db.spark.find().sort({"list": 1})
+# periodically clean it
+db.spark.remove({})
 ```
 
 You can also do it using netcat
@@ -27,7 +36,9 @@ You can also do it using netcat
 # start netcat server
 docker exec -it web-app nc -l -p 9999
 # open second docker terminal and submit Spark job:
-docker exec -it web-app /usr/spark-2.1.0/bin/spark-submit  /opt/jar/target/spark-streaming-jar-module.jar
+docker exec -it web-app /usr/spark-2.1.0/bin/spark-submit \
+--jars /opt/mongo-spark-connector_2.11-2.0.0.jar,/opt/mongo-java-driver-3.4.2.jar \
+/opt/jar/target/spark-streaming-jar-module.jar
 ```
 
 
@@ -44,8 +55,8 @@ sudo mount -t vboxsf spark-streaming-module-web /d/projects/configuration-profil
 
 ```sh
 docker exec -it web-app /bin/bash
-docker stop spark-master spark-worker web-app
-docker rm spark-master spark-worker web-app
+docker stop spark-master spark-worker spark-worker2 web-app mongo
+docker rm spark-master spark-worker spark-worker2 web-app mongo
 /usr/spark-2.1.0/bin/spark-submit  /opt/jar/target/spark-streaming-jar-module.jar
 ```
 
--- configuration-profiles/docker-spark-streaming/spark-streaming-module-jar/src/main/java/com/example/SparkMain.java
+++ configuration-profiles/docker-spark-streaming/spark-streaming-module-jar/src/main/java/com/example/SparkMain.java
@@ -1,11 +1,24 @@
 package com.example;
 
+import com.mongodb.spark.MongoSpark;
+
 import org.apache.spark.*;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
 import org.apache.spark.api.java.function.*;
 import org.apache.spark.streaming.*;
 import org.apache.spark.streaming.api.java.*;
 import scala.Tuple2;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
 
 /**
  * @author Aleksandr_Savchenko
@@ -13,19 +26,23 @@ import scala.Tuple2;
 public class SparkMain {
 
     public static void main(String[] args) throws Exception {
-        wordCountJava8();
+        runStreaming();
     }
 
-    public static void wordCountJava8() throws InterruptedException {
+    public static void runStreaming() throws InterruptedException {
         // Define a configuration to use to interact with Spark
         SparkConf conf = new SparkConf()
                 .setMaster("spark://spark-master:7077")
                 .setAppName("My Streaming Count App")
+                .set("spark.mongodb.output.uri", "mongodb://mongo-host:27017/")
+                .set("spark.mongodb.output.database", "local")
+                .set("spark.mongodb.output.collection", "spark")
                 //.set("spark.executor.memory", "1024m")
                 //.set("spark.cores.max", "1")
                 //.set("spark.default.parallelism", "3")
                 ;
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
+        jssc.checkpoint(".");
         JavaReceiverInputDStream<String> numbers = jssc.socketTextStream("web-app", 9999);
 
         JavaPairDStream<Integer, Integer> pairs = numbers.mapToPair(
@@ -51,18 +68,68 @@ public class SparkMain {
                         return integerIntegerJavaPairRDD.sortByKey(true);
                     }
                 });
-
-        sortedStream.map(new Function<Tuple2<Integer,Integer>, String>() {
+        /*
+        // printing results in nice view
+        sortedStream.map(new Function<Tuple2<Integer, Integer>, String>() {
             @Override
             public String call(Tuple2<Integer, Integer> tupple) throws Exception {
                 String template = "(%d .. %d, %d)";
                 return String.format(template, tupple._1, tupple._1 + 10, tupple._2);
             }
         }).print();
+        */
+
+
+        /*
+        // collecting overall statistics, don't forget to set-up shared FS (HDFS or S3)
+        JavaPairDStream<Integer, Integer> sumStream = sortedStream.updateStateByKey(
+                new Function2<List<Integer>, Optional<Integer>, Optional<Integer>>() {
+                    @Override
+                    public Optional<Integer> call(List<Integer> integers, Optional<Integer> current) throws Exception {
+                        int currentSum = current.or(0) + integers.stream().reduce(0, (x, y) -> x + y);
+                        return Optional.of(currentSum);
+                    }
+                });
+
+        */
+
+
+        sortedStream.foreachRDD(new VoidFunction2<JavaPairRDD<Integer, Integer>, Time>() {
+            @Override
+            public void call(JavaPairRDD<Integer, Integer> integerIntegerJavaPairRDD, Time time) throws Exception {
+                if (!integerIntegerJavaPairRDD.isEmpty()) {
+
+                    JavaRDD<Document> rdd = integerIntegerJavaPairRDD.map(new Function<Tuple2<Integer, Integer>, Document>() {
+                        @Override
+                        public Document call(Tuple2<Integer, Integer> tupple) throws Exception {
+                            return new Document(String.valueOf(tupple._1), tupple._2);
+                        }
+                    });
+
+
+                    Document doc = new Document("_id", new ObjectId(new Date(time.milliseconds())));
+                    Document reduced = rdd.reduce(new Function2<Document, Document, Document>() {
+                        @Override
+                        public Document call(Document document, Document document2) throws Exception {
+                            // merging, didn't find any better way for that
+                            document2.keySet().stream().forEach(key -> {
+                                document.put(key, document2.get(key));
+                            });
+                            return document;
+                        }
+                    });
+                    doc.append("list", reduced);
+
+                    List<Document> list = Collections.singletonList(doc);
+                    JavaRDD<Document> r = JavaSparkContext.fromSparkContext(integerIntegerJavaPairRDD.context()).parallelize(list);
+                    MongoSpark.save(r);
+                    //integerIntegerJavaPairRDD.coalesce(1).saveAsTextFile("/mydata");
+                }
+            }
+        });
 
         jssc.start();              // Start the computation
         jssc.awaitTermination();   // Wait for the computation to terminate
     }
-
 
 }
