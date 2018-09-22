c24d600127e86a352d9dee3fa622c0d7a0da5abc
yeahhoo <sasa.89@bk.ru>
2018-09-22 23:39:45 +0300

    learning Scala: implemented IEEE754 algorithm

--- scala-tasks/algorithms/src/main/scala/com/example/binary_ops/BinaryOps.scala
+++ scala-tasks/algorithms/src/main/scala/com/example/binary_ops/BinaryOps.scala
@@ -1,35 +1,141 @@
 package com.example.binary_ops
 
-
+/**
+	* Contains functions to operate on bits. Inspired by:
+	* http://www.oxfordmathcenter.com/drupal7/node/43
+	* https://www.h-schmidt.net/FloatConverter/IEEE754.html
+	* */
 object BinaryOps {
 
-  // index is 1 based
-  def findBitByIndex(value : Int, index : Int) : Int = {
-    (value >> index - 1) & 1
+	/** Finds value of bit in given number by index. Index is 0 based */
+	def findBitByIndex(value: Int, index: Int): Int = {
+		(value >> index) & 1
+	}
+
+	/** Finds range of bits in given number by start and end indexes. Start and end are 0 based */
+	def findBitsByRange(value: Int, start: Int, end: Int): Int = {
+		val startedDropped = value >> start
+		val mask = (~0 & (1 << (end - start + 1))) - 1
+		startedDropped & mask
+	}
+
+	/** Replaces range of bits in given number with specified bits. Start and end are 0 based */
+	def replaceBitsByRange(value: Int, replacement: Int, start: Int, end: Int): Int = {
+		val max = ~0
+		val leftPart = (((1 << start) - 1) & max) & value
+		val maxRightOffset = max - ((1 << end + 1) - 1)
+		val rightWithAddedReplacement = (value & maxRightOffset) | (replacement << start)
+		return rightWithAddedReplacement | leftPart
+	}
+
+	/** Converts given number from 10-base to 2-base system. */
+	def toBinary(n: Long): String = {
+		n match {
+			case 0 => "0"
+			case 1 => "1"
+			case _ => toBinary(n / 2) + (n % 2).toString
+		}
+	}
+
+	/** Converts given number from 2-base to 10-base system. */
+  def fromBinary(s : String) : Long = {
+		import scala.math.pow
+		s match {
+			case "0" => 0
+			case "1" => 1
+			case _ => (Integer.parseInt(s(0).toString) * pow(2, s.length - 1)).toLong + fromBinary(s.substring(1))
+		}
   }
 
-  // start and end are 1 based
-  def findBitsByRange(value : Int, start : Int, end : Int) : Int = {
-    val startedDropped = value >> start - 1
-    val mask = ~(1 << end)
-    startedDropped & mask
-  }
+	/** Converts positive and negative decimal numbers to binary format with given capacity. */
+	def toBinaryWithNegativeValues(n: Int, capacity: Int): String = {
+		def _plusOne(v: String): String = {
+			if (v == "") return ""
+			else if (v(v.length - 1) == '0') return v.substring(0, v.length - 1) + "1"
+			else return _plusOne(v.substring(0, v.length - 1)) + "0"
+		}
 
-  // start and end are 1 based
-  def replaceBitsByRange(value : Int, replacement : Int, start : Int, end : Int) : Int = {
-    val replBits = findBitsByRange(replacement, 1, end - start + 1)
-    val right = ((1 << start - 1) - 1)
-    val left = ~0 - ((1 << end) - 1)
-    val mask = right | left
-    (value & mask) | (replBits << start - 1)
-  }
+		if (n >= 0) return List.fill(capacity - toBinary(n).length)(0).mkString + toBinary(n)
+		val numberOfFreeBits = capacity - toBinary(-1 * n).length
+		val binPositiveValue = List.fill(numberOfFreeBits)(0).mkString + toBinary(-1 * n)
+		val revertedValue = binPositiveValue.map(i => if (i == '0') "1" else "0").mkString
+		_plusOne(revertedValue)
+	}
 
-  def main(args: Array[String]): Unit = {
-    val value = 15
-    val replacement = 5
-    val bits = replaceBitsByRange(value, replacement, 3, 5)
-    println(s"value: ${value.toBinaryString}")
-    println(s"replacement: ${replacement.toBinaryString}")
-    println(s"result: ${bits.toBinaryString}")
-  }
+	/** Represents given float number in binary format according to IEEE754 standard. */
+	def toIEEE754BinaryFormat(value: Float): String = {
+
+		def _findMantissa(value: Double, roundLimit: Int): String = {
+			if (roundLimit == 0) return ""
+			value match {
+				case 1.0 => return "1"
+				case _ => {
+					val nextValue = value * 2
+					if (nextValue == 1) "1"
+					else if (nextValue > 1) "1" + _findMantissa(nextValue - 1, roundLimit - 1)
+					else "0" + _findMantissa(nextValue, roundLimit - 1)
+				}
+			}
+		}
+
+		def _formalizeValue(value: Float, shift: Int = 0): (Float, Int) = {
+			if (value >= 1 && value < 2) return (value, shift)
+			if (value > 1) return _formalizeValue(value / 2, shift + 1)
+			else _formalizeValue(value * 2, shift - 1)
+		}
+
+		val sign = if (value < 0) "1" else "0"
+		val formalizedValue = _formalizeValue(scala.math.abs(value))
+
+		val initCapacity = 127
+		val exponentBinary = toBinaryWithNegativeValues(initCapacity + formalizedValue._2, 8)
+
+		val partMan = _findMantissa(formalizedValue._1 - 1, 23)
+		val mantissaBinary = partMan + List.fill(23 - partMan.length)(0).mkString
+		return sign + " " + exponentBinary + " " + mantissaBinary
+	}
+
+	/** Converts given number presented in binary format back to float according to IEEE754 standard. */
+	def fromIEEE754BinaryFormat(binStr: String): Float = {
+		val value = binStr.replaceAll("\\s", "")
+		val sign = if (value(0) == '0') 1 else -1
+		val expStr = value.substring(1, 9)
+		val manStr = value.substring(9, 32)
+		val bias = (fromBinary(expStr) - 127).toInt
+		val decValue: Float = manStr.view.zipWithIndex.toList.foldLeft(0f)((a, p) => {
+			val m = if (p._1 == '1') 1 else 0
+			a + m * scala.math.pow(2, -1 * p._2.toInt - 1).toFloat
+		})
+		return sign * (1 + decValue) * scala.math.pow(2, bias).toFloat
+	}
+
+	private def convertValueTest(value: Float) = {
+		val binValue = toIEEE754BinaryFormat(value)
+		val restoredDecValue = fromIEEE754BinaryFormat(binValue)
+		val difference = value - restoredDecValue
+
+		println(s"---------- TEST -----------")
+		println(s"decValue: ${value}")
+		println(s"binValue: ${binValue}")
+		println(s"restoredDecValue: ${restoredDecValue}")
+		println(s"difference: ${difference}")
+	}
+
+	def main(args: Array[String]): Unit = {
+		val value = 85
+		val replacement = 7
+		val bits = replaceBitsByRange(value, replacement, 2, 4)
+		println(s"value: ${value.toBinaryString}")
+		println(s"replacement: ${replacement.toBinaryString}")
+		println(s"result: ${bits.toBinaryString}")
+
+		convertValueTest(23.125f)
+		convertValueTest(-4.87f)
+		convertValueTest(0.14f)
+		convertValueTest(-0.23f)
+		convertValueTest(100000000000.23f)
+		convertValueTest(10000000000.23231f)
+		convertValueTest(8000000000.384f)
+		convertValueTest(0.00030f)
+	}
 }
